08a1039d91c3bbd1833ad47801daf66d9cc2c8e0
Kiara Johnson <johns.kia35@gmail.com>
2025-07-12 21:20:14 -0600

    Add Service Worker

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
-const VERSION = "v1"
+// Cache Version
+const VERSION = "v1";
+// Cache Name
+const CACHE_NAME = `cycle-tracker-${VERSION}`;
+
+//Static resources for functionality
+const APP_STATIC_RESOURCES = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/circleicon-512-512.png",
+];
+
+//Saving cache on install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })()
+  );
+});
+
+//Delete old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const names = await caches.keys();
+    await Promise.all(
+      names.map((name) => {
+        if (name !== CACHE_NAME) {
+          return caches.delete(name);
+        }
+        return undefined;
+      }),
+    );
+    await clients.claim();
+  })(),
+  );
+});
+
